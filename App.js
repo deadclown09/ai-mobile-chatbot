476764ecb8d888bd071736ab0c6ddea9ddbd5fe2
Sandesh Katwal <mesandesh.2014@gmail.com>
2023-06-13 15:25:03 +0545

    feat: intialized AI Mobile Chatbot project

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Homescreen from './app/pages/Homescreen';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Homescreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
